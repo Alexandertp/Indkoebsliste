925fd48106d4c425fe7c355a55e4c90bea1952e5
KMS1150 <futty1155@gmail.com>
2025-09-15 13:54:37 +0200

    add function too read from file

--- Indkoebsliste/Program.cs
+++ Indkoebsliste/Program.cs
@@ -1,51 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
 string vareFilNavn = "vareFil.txt";
+Vare[] produktListe = ReadFromFile();
 
 
-Vare[] produktListe = new Vare[10];
-produktListe[0] = new Vare();
-produktListe[1] = new Vare();
-produktListe[2] = new Vare();
-produktListe[3] = new Vare();
-produktListe[4] = new Vare();
-produktListe[5] = new Vare();
-produktListe[6] = new Vare();
-produktListe[7] = new Vare();
-produktListe[8] = new Vare();
-produktListe[9] = new Vare();
 
-produktListe[0].id = 0;
-produktListe[1].id = 1;
-produktListe[2].id = 2;
-produktListe[3].id = 3;
-produktListe[4].id = 4;
-produktListe[5].id = 5;
-produktListe[6].id = 6;
-produktListe[7].id = 7;
-produktListe[8].id = 8;
-produktListe[9].id = 9;
 
-produktListe[0].navn = "Burger";
-produktListe[1].navn = "Frikadelle";
-produktListe[2].navn = "Brød";
-produktListe[3].navn = "Tandpasta";
-produktListe[4].navn = "Toiletpapir";
-produktListe[5].navn = "Vingummi";
-produktListe[6].navn = "Smør";
-produktListe[7].navn = "Lim";
-produktListe[8].navn = "Hakkekød";
-produktListe[9].navn = "Grød";
 
-produktListe[0].pris = 20;
-produktListe[1].pris = 30;
-produktListe[2].pris = 40;
-produktListe[3].pris = 50;
-produktListe[4].pris = 60;
-produktListe[5].pris = 70;
-produktListe[6].pris = 80;
-produktListe[7].pris = 90;
-produktListe[8].pris = 100;
-produktListe[9].pris = 200;
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
 
 Console.WriteLine("Hvad vil du købe?");
 List<Vare> indkøbsVogn = new List<Vare>();
@@ -95,6 +78,32 @@ void ListVarer()
         Console.WriteLine(vare.id + ". " + vare.navn + " Pris: " +  vare.pris +"kr");
     }
 }
+
+Vare[] ReadFromFile()                                        
+{                                                          
+    if (!File.Exists(vareFilNavn))                         
+    {                                                      
+        File.Create(vareFilNavn);                          
+    }                                                      
+    string[] importTekst = File.ReadAllLines(vareFilNavn); 
+    Vare[] produktListe = new Vare[importTekst.Length];    
+    int i = 0;                                             
+                                                           
+    foreach (string line in importTekst)                   
+    {                                                      
+        Console.WriteLine(line);                           
+        string[] opdelt = line.Split(',');                 
+        produktListe[i] = new Vare();                      
+        produktListe[i].navn  = opdelt[0];                 
+        produktListe[i].pris = Convert.ToDouble(opdelt[1]);
+        produktListe[i].id  = Convert.ToInt32(opdelt[2]);  
+        Console.WriteLine("Taget fra VareArray:");         
+        Console.WriteLine(produktListe[i]);                
+                                                           
+        i++;                                               
+    }
+    return produktListe;
+}                                                          
 
 public class Vare()
 {
